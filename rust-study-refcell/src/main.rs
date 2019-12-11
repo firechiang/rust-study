f6474c3e2bfb1c534bce415d6aeb4ee58d8f94ba
jiangcihuo <chiang-fire@outlook.com>
2019-12-11 18:35:13 +0800

    feat: 弱引用简单使用（如果a引用b，b再引用a，恰巧又是强引用的话就会造成栈溢出。使用弱引用就没有这个问题）

---
+++ rust-study-refcell/src/main.rs
@@ -0,0 +1,32 @@
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+
+/**
+ * 弱引用简单使用（如果a引用b，b再引用a，恰巧又是强引用的话就会造成栈溢出。使用弱引用就没有这个问题）
+ */
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
