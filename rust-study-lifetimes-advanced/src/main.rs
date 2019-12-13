cf90c50ae56060270f5869e6081400e059d7b3e1
jiangcihuo <chiang-fire@outlook.com>
2019-12-13 10:35:07 +0800

    fix: 生命周期统一简单使用（两个变量的生命周期可以一样长，只要一个还存在，另一个就不会被销毁）

--- rust-study-lifetimes-advanced/src/main.rs
+++ rust-study-lifetimes-advanced/src/main.rs
@@ -2,4 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct Context<'a>(&'a str);
 
+struct Parse<'a> {
+    context: &'a Context<'a>
+}
+
+impl<'a> Parse<'a> {
+    fn parse (&self) -> Result<(),&'a str>{
+        return Err(&self.context.0[1..]);
+    }
+}
