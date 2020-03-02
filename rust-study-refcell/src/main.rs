ee19685a81ba2b55bec0294a9d37ac59c29db52d
jiangcihuo <chiang-fire@outlook.com>
2020-03-02 16:53:19 +0800

    fix: 30->弱引用简单使用（如果a引用b，b再引用a，恰巧又是强引用的话就会造成栈溢出。使用弱引用就没有这个问题）

--- rust-study-refcell/src/main.rs
+++ rust-study-refcell/src/main.rs
@@ -6,6 +6,7 @@ use std::cell::RefCell;
  */
 fn main() {
 
+    // 孩子对象
     let leaf = Rc::new(Node {
         value: 3,
         parent: RefCell::new(Weak::new()),
@@ -13,12 +14,15 @@ fn main() {
     });
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
+    // 父亲对象
     let branch = Rc::new(Node {
         value: 5,
         parent: RefCell::new(Weak::new()),
+        // 孩子是 leaf
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    // 注意：downgrade函数是获取branch的弱引用
     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
