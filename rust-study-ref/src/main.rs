c5119250fd378ef31558bf67863874a85a30f58c
jiangcihuo <chiang-fire@outlook.com>
2020-03-02 15:42:46 +0800

    fix: 28->销毁内存回调简单使用，*号和&号获取引用是调用Deref接口的deref函数取值

--- rust-study-ref/src/main.rs
+++ rust-study-ref/src/main.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 /**
- * 使用引用回调，销毁内存回调
+ * 销毁内存回调简单使用，*号和&号获取引用是调用Deref接口的deref函数取值
  */
 fn main() {
     // 使用引用就回调
@@ -10,7 +10,7 @@ fn main() {
 
     // 销毁内存就回调
     let c = CustomSmartPointer { data: String::from("my stuff") };
-    // 强制销毁内存
+    // 强制销毁内存（实际就是直接调用drop函数）
     //drop(c);
     let d = CustomSmartPointer { data: String::from("other stuff") };
     println!("CustomSmartPointers created.");
@@ -33,6 +33,7 @@ impl<T> Deref for MyBox<T> {
     type Target = T;
 
     // 只要代码里面使用了MyBox的引用就会调用这个函数
+    // 注意：只要获取MyBox的引用都会调用这个函数取值，比如*MyBox,&MyBox实际调用的就是这个函数
     fn deref(&self) -> &T {
         println!("使用引用了");
         &self.0
